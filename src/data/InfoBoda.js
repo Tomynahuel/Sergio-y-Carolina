a63d3d38a24d7b6c87d6446da221be483347561b
tomas <tomynahuel23@gmail.com>
2026-07-17 18:32:04 +0000

    Nuevas cosas

---
+++ src/data/InfoBoda.js
@@ -0,0 +1,43 @@
+
+
+export const BODA_INFO = {
+  nombres: "Sergio & Carolina",
+
+  novio: "Sergio",
+  novia: "Carolina",
+
+  fecha: "3 de Octubre de 2026",
+  fechaCuentaRegresiva: "2026-10-03T15:00:00", // Para el contador
+
+  //civil
+  
+  lugarCivil: "lorem ipsum",
+  direccionCivil: "lorem ipsum",
+  direccionCeremonia: "lorem ipsum",
+  fechaCivil: "3",
+  mesCivil: "Octubre",
+  horaCivil: "15:00",
+
+
+  //ceremonia
+  fechaCeremonia: "3",
+  mesCeremonia: "Octubre",
+  horaCeremonia: "11:00",
+  lugarCeremonia: "lorem ipsum",
+  direccionCeremonia: "lorem ipsum",
+  horarioCeremonia: "20:00",
+  lugarCeremonia: "lorem ipsum",
+
+  //festejo
+  fechaFestejo: "3",
+  mesFestejo: "Octubre",
+  horaFestejo: "20:00",
+  lugarFestejo: "lorem  ipsum",
+  direccionFestejo: "lorem ipsum" ,
+
+  aliasBoda: "boda.sergio.caro",
+  cbu: "lorem ipsum",
+
+
+  whatsappContacto: "+5491123456789"
+};
