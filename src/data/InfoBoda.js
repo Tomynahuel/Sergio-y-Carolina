90b20ba410e1430a007d39e3653aaac4e3ba85dc
tomas <tomynahuel23@gmail.com>
2026-08-04 16:54:17 -0300

    Ajustes 3

--- src/data/InfoBoda.js
+++ src/data/InfoBoda.js
@@ -3,11 +3,13 @@
 export const BODA_INFO = {
   nombres: "Sergio & Carolina",
 
-  novio: "Sergio",
-  novia: "Carolina",
+  novio: "Sergio", contactoNovio: "",
+  novia: "Carolina", contactoNovia: "",
 
   fecha: "3 de Octubre de 2026",
   fechaCuentaRegresiva: "2026-10-03T15:00:00", // Para el contador
+
+
 
   //civil
   
@@ -27,17 +29,41 @@ export const BODA_INFO = {
   direccionCeremonia: "lorem ipsum",
   horarioCeremonia: "20:00",
   lugarCeremonia: "lorem ipsum",
+  salonCeremonia: "lorem ipsum",
 
-  //festejo
-  fechaFestejo: "3",
-  mesFestejo: "Octubre",
-  horaFestejo: "20:00",
-  lugarFestejo: "lorem  ipsum",
-  direccionFestejo: "lorem ipsum" ,
+  //recepción
+  fechaRecepcion: "3",
+  mesRecepcion: "Octubre",
+  horaRecepcion: "18:00",
+  lugarRecepcion: "lorem ipsum",
+  direccionRecepcion: "lorem ipsum",
+  
+  //fecha limites
+  fechaLimite:"15",
+  mesLimite:"Septiembre",
 
+  // Contacto
+  contacto1:"Tomas Aguilar",
+  emailContacto1:"tomynahuel23@gmail.com",
+  numeroTelefono1:"+5491123456789",
+  contacto2: "Ana Belen Baez Barroso",
+  emailContacto2: "", 
+  numeroTelefono2: "+5491123456789",
+  
+
+  // Vestimenta
+  vestimenta: "lorem ipsum",
+
+
+  // Regalos
+  infoCuenta: "Si querés colaborar con nuestro viaje de novios, podés hacerlo a través de la siguiente cuenta bancaria:",
+  dueñoCuenta: "Sergio Caro",
   aliasBoda: "boda.sergio.caro",
   cbu: "lorem ipsum",
 
+  // Ubicacion 
+  ubiEvento: "Parque de las naciones, Obera", // Link de Google Maps
+  linkMaps: "https://www.google.com/maps?q=Parque+de+las+Naciones+Ober%C3%A1&output=embed",
 
   whatsappContacto: "+5491123456789"
 };
